6c0d9ae77a4ae260ddaa2d38ba9635f9284ab804
Priyanshu Jain <priyanshujain831@gmail.com>
2021-11-27 19:39:21 +0530

    next

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import aboutlogo from "./site_assets/brand_logo_1.png";
+import { NavLink, Link } from "react-router-dom";
+import "./CSS/about.css";
 
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <h1 style={{ textAlign: "center" }} className="pt-5">
-          This feature is under construction...
-        </h1>
-        <h3 style={{ textAlign: "center" }}>Sorry for inconvinience </h3>
+      <div className="d-flex justify-content-center about_bg container-fluid">
+        <section>
+          <NavLink to="/">
+            <img src={aboutlogo} className="about_brand" />
+          </NavLink>
+        </section>
+        <section>
+          <h5>About us</h5>
+        </section>
       </div>
     );
   }
--- src/components/liveSessions.jsx
+++ src/components/liveSessions.jsx
@@ -10,51 +10,48 @@ class LiveSessions extends React.Component {
     Aos.init();
     return (
       <section className="livesession-bg d-flex align-items-center justify-content-center">
-        <div data-aos="fade-up" data-aos-delay="500" data-aos-duration="1500">
-          <div className=" login-card">
-            <section className="container-fluid">
-              <section className="row align-items-center">
-                <section className="col-lg">
-                  <form className="form-container">
-                    <div className="form-group">
-                      <NavLink className="navbar-brand login-logo" to="/">
-                        <img src={sitelogo} className="navbar-brand" alt="" />
-                      </NavLink>
-                      <h2 className="text-center font-weight-bold"> Login </h2>
-                      <h4 className="text-center font-weight-bold">
-                        You need to be logged in to watch the live session
-                      </h4>
-                      <input
-                        type="email"
-                        className="form-control mt-4 my-2"
-                        id="InputEmail1"
-                        aria-describeby="emailHelp"
-                        placeholder="Enter email"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <input
-                        type="password"
-                        className="form-control my-2"
-                        id="InputPassword1"
-                        placeholder="Password"
-                      />
-                    </div>
-                    <Link
-                      to="/live-sessions"
-                      type="submit"
-                      className="btn btn-primary btn-block my-4"
-                    >
-                      Login
-                    </Link>
-                    <p>
-                      Don't have an account? <Link to="/">Register now!</Link>
-                    </p>
-                  </form>
-                </section>
-              </section>
-            </section>
-          </div>
+        <div
+          data-aos="fade-up"
+          data-aos-delay="500"
+          data-aos-duration="1500"
+          className=" login-card"
+        >
+          <section className="container-fluid">
+            <form className="form-container">
+              <div className="form-group">
+                <NavLink className="navbar-brand login-logo" to="/">
+                  <img src={sitelogo} className="navbar-brand" />
+                </NavLink>
+                <h2 className="text-center font-weight-bold"> Login </h2>
+                <h4 className="text-center font-weight-bold">
+                  You need to be logged in to watch the live session
+                </h4>
+                <input
+                  type="email"
+                  className="form-control mt-4 my-2"
+                  id="InputEmail1"
+                  aria-describeby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <input
+                  type="password"
+                  className="form-control my-2"
+                  id="InputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+              <Link
+                to="/live-sessions"
+                type="submit"
+                className="btn btn-primary btn-block my-4"
+              >
+                Login
+              </Link>
+              <p>
+                Don't have an account? <Link to="/">Register now!</Link>
+              </p>
+            </form>
+          </section>
         </div>
       </section>
     );
