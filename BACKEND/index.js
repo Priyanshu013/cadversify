ffbacf21b82dc384fecc2c6b76eb9358d1a4fd93
Priyanshu Jain <priyanshujain831@gmail.com>
2022-01-01 23:04:00 +0530

    next

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -8,6 +8,21 @@ mongoose
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  //checking if the course exists or not
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given id was not found");
+  else res.send(course);
 });
 
 const port = process.env.PORT || 5000;
